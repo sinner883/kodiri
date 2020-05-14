4161425349c5892743c4bbc9a97400ed2c8774e5
sinner883 <sinner883@users.noreply.github.com>
2020-05-14 23:13:50 +0100

    removed the gap between picts on desktop version

--- src/Details.js
+++ src/Details.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Details() {
     return (
         <div>
-            <h1>This page is in progress!</h1>
+            <h3>This page is in progress!</h3>
             <Link to='/'>Go back!</Link>
         </div>
     )
