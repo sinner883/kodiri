036835902e19867810147139325f70806c4a3952
sinner883 <sinner883@users.noreply.github.com>
2020-05-16 17:19:18 +0100

    complited issue #11 and #12 where added css grid layout, moved the cover data model

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import Stack from './Stack.js'
-import matrix1 from './img/matrix1.jpg';
-import matrix2 from './img/matrix2.jpg';
-import matrix3 from './img/matrix3.jpg';
-import war from './img/war.jpg';
-import prodigy from './img/prodigy.jpg';
-import glass from './img/glass.jpg';
+import Stack from './Stack.js';
+import getMovieApp from './getMoiveApp';
 
 export default function MovieApp() {
     return (
         <div>
             <h1>My Library</h1>
             <div className='container'>
-                <Stack id='matrix' name='The Matrix' logo={matrix1} />
-                <Stack id='matrix2' name='The Matrix: Reloaded' logo={matrix2} />
-                <Stack id='matrix3' name='The Matrix: Revolutions' logo={matrix3} />
-            </div>
-            <div className='container'>
-                <Stack id='war' name='War' logo={war} />
-                <Stack id='prodigy' name='Prodigy' logo={prodigy} />
-                <Stack id='glass' name='Glass' logo={glass} />
+               {
+                   getMovieApp().map(stack => (
+                       <Stack 
+                        key={stack.id}
+                        id={stack.id}
+                        name={stack.name} 
+                        logo={stack.logo} />
+                   ))
+               } 
             </div>
         </div>
 
---
+++ src/getMoiveApp.js
@@ -0,0 +1,17 @@
+import matrix1 from './img/matrix1.jpg';
+import matrix2 from './img/matrix2.jpg';
+import matrix3 from './img/matrix3.jpg';
+import war from './img/war.jpg';
+import prodigy from './img/prodigy.jpg';
+import glass from './img/glass.jpg';
+
+export default function getMovieApp() {
+    return[
+        {id: 'matrix', name: 'The Matrix', logo: matrix1 },
+        {id: 'matrix2', name: 'The Matrix: Reloaded', logo: matrix2 },
+        {id: 'matrix3', name: 'The Matrix: Revolutions', logo: matrix3 },
+        {id: 'war', name: 'War', logo: war },
+        {id: 'prodigy', name: 'The Prodigy', logo: prodigy },
+        {id: 'glass', name: 'Glass', logo: glass }
+    ];
+}
