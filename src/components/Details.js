dd73b4096f239294cc153330ca5e5d806d32e243
sinner883 <sinner883@users.noreply.github.com>
2020-05-29 00:16:31 +0100

    reusing existing css classes

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import getMovie from './getMovieApp';
+import './Details.css';
 
 export default class Details extends Component {
 
@@ -22,21 +23,25 @@ export default class Details extends Component {
     }
 
     render() {
-        if (this.state.movie === undefined){
-            return <Redirect to = '/notFound' />;
+        if (this.state.movie === undefined) {
+            return <Redirect to='/notFound' />;
         } else {
             return (
-                <div>
+                <div className="Details">
                     <h3>{this.state.movie.name}</h3>
-                    <div>{this.state.movie.syponsis}</div>
-                    <img 
-                        src={this.state.movie.logo} 
+                    <div className="details__container">{this.state.movie.syponsis}
+                    </div>
+                    <img
+                        src={this.state.movie.logo}
                         alt={this.state.movie.name} />
-                    <Link to='/'>Go back!</Link>
+                    <div>
+                        <Link to='/'>Go back!</Link>
+                    </div>
                 </div>
+
             )
         }
-        
+
     }
 }
 // 1. introducing the component state
